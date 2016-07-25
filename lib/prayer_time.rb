80b674ebb4a5da268b7e2cde580eeab2358319a0
ecmel <ecmel.kytz@gmail.com>
2016-07-25 10:52:59 +0300

    set to requests

--- lib/prayer_time.rb
+++ lib/prayer_time.rb
@@ -1,5 +1,17 @@
 require "prayer_time/version"
+require "prayer_time/request"
 
 module PrayerTime
-  # Your code goes here...
+
+  def self.cities(country)
+    Request.new.get_cities(country)
+  end
+
+  def self.towns(country, city)
+    Request.new.get_towns(country, city)
+  end
+
+  def self.times(country, city, town)
+    Request.new.pray_times(country, city, town)
+  end
 end
